c0aaef83ae5967e423d389947fc1867ba9e05cc8
Fregatov Leonid <fregatov163@gmail.com>
2019-04-12 17:06:38 +0400

    two commit of the project

---
+++ src/AIT4/Fregatov/wdad/Application.java
@@ -0,0 +1,7 @@
+package AIT4.Fregatov.wdad;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("I’m Leonid Fregatov, and I’m not a monkey");
+    }
+}
